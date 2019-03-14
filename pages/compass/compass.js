8ec0709ffef20917e19b7aa07e9af6f6e937a54d
ls_003 <759538462@qq.com>
2019-03-14 17:41:44 +0800

    手电筒, 罗盘, 指南针

---
+++ pages/compass/compass.js
@@ -0,0 +1,122 @@
+// pages/compass/compass.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+	data: {
+		position: "",
+		positionNumber: 0,
+		accuracy: "",
+	},
+  /**
+   * 生命周期函数--监听页面加载
+   */
+	onLoad: function (options) {
+		/* 
+		{accuracy:"high", direction:253.93016}
+		*/
+		wx.startCompass({
+			success: () => {
+				console.log("成功")
+				wx.onCompassChange(res => {
+					if (res.accuracy == "high" || res.accuracy == "medium" || res.accuracy) {
+						this.setData({
+							positionNumber: parseInt(res.direction)
+						})
+						switch (true) {
+							case res.direction == 0:
+								this.fnSetData("北方")
+								break;
+							case res.direction == 90:
+								this.fnSetData("东方")
+								break;
+							case res.direction == 180:
+								this.fnSetData("南方")
+								break;
+							case res.direction == 270:
+								this.fnSetData("西方")
+								break;
+							case res.direction > 0 && res.direction < 90:
+								this.fnSetData("东北方")
+								console.log(this)
+								break;
+							case res.direction > 90 && res.direction < 180:
+								this.fnSetData("东南方")
+								console.log(this)
+								break;
+							case res.direction > 180 && res.direction < 270:
+								this.fnSetData("西南方")
+								console.log(this)
+								break;
+							case res.direction > 270:
+								this.fnSetData("西北方")
+								console.log(this)
+								break;
+						}
+					}
+				})
+			},
+			fail() {
+				console.log("失败")
+			},
+			complete() {
+				console.log("调用了罗盘")
+			}
+		})
+	},
+	fnSetData: function (po) {
+		console.log(po)
+		this.setData({
+			position: po
+		})
+	},
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+	onReady: function () {
+
+	},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+	onShow: function () {
+
+	},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+	onHide: function () {
+
+	},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+	onUnload: function () {
+
+	},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+	onPullDownRefresh: function () {
+
+	},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+	onReachBottom: function () {
+
+	},
+
+  /**
+   * 用户点击右上角分享
+   */
+	onShareAppMessage: function () {
+
+	}
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -9,19 +9,31 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  //事件处理函数
-  bindViewTap: function() {
+  //跳转手电筒
+  goToLight: function() {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../light/light'
     })
   },
-  onLoad: function () {
+  //跳转镜子
+  goToMirror: function() {
+    wx.navigateTo({
+      url: '../mirror/mirror'
+    })
+  },
+  //跳转指南针
+  goToCompass: function() {
+    wx.navigateTo({
+      url: '../compass/compass'
+    })
+  },
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -50,5 +62,10 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
+  },
+  onGotUserInfo(e) {
+    console.log(e.detail.errMsg)
+    console.log(e.detail.userInfo)
+    console.log(e.detail.rawData)
+  },
 })
